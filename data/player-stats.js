a60e419270bac181c4c19014f5fe2f4457275946
choong-syu <128004136+choong-syu@users.noreply.github.com>
2026-08-04 16:33:05 +0000

    Update KBO data

--- data/player-stats.js
+++ data/player-stats.js
@@ -1,7 +1,7 @@
 window.KBO_PLAYER_STATS = {
   "season": 2026,
-  "asOf": "2026년 8월 4일 23시 기준",
-  "fetchedAt": "2026-08-04T14:33:49.794Z",
+  "asOf": "2026년 8월 5일 01시 기준",
+  "fetchedAt": "2026-08-04T16:33:05.090Z",
   "source": {
     "roster": "https://www.koreabaseball.com/Player/Register.aspx",
     "hitterBasic": "https://www.koreabaseball.com/Record/Player/HitterBasic/Basic1.aspx",
@@ -821,7 +821,7 @@ window.KBO_PLAYER_STATS = {
       "walksHitByPitch": "33",
       "ops": "0.691",
       "hits": "74",
-      "rbi": "35",
+      "rbi": "34",
       "sluggingPercentage": "0.347",
       "strikeouts": "53"
     },
@@ -1581,9 +1581,9 @@ window.KBO_PLAYER_STATS = {
       "holds": "0",
       "saves": "0",
       "innings": "25 2/3",
-      "era": "4.91",
+      "era": "4.21",
       "strikeouts": "16",
-      "whip": "1.71",
+      "whip": "1.68",
       "qs": "0"
     },
     {
@@ -3738,15 +3738,15 @@ window.KBO_PLAYER_STATS = {
       "type": "hitter",
       "pitchBat": "우투좌타",
       "transfer": "NC 다이노스 (2013~2015)<br>NC 다이노스 (2017~2022)<br>롯데 자이언츠 (2023~)",
-      "battingAverage": "0.227",
+      "battingAverage": "0.222",
       "atBats": "176",
       "homeRuns": "5",
-      "onBasePercentage": "0.307",
+      "onBasePercentage": "0.302",
       "walksHitByPitch": "22",
-      "ops": "0.682",
-      "hits": "40",
+      "ops": "0.671",
+      "hits": "39",
       "rbi": "25",
-      "sluggingPercentage": "0.375",
+      "sluggingPercentage": "0.369",
       "strikeouts": "40"
     },
     {
@@ -3785,7 +3785,7 @@ window.KBO_PLAYER_STATS = {
       "holds": "0",
       "saves": "0",
       "innings": "112",
-      "era": "4.10",
+      "era": "4.02",
       "strikeouts": "131",
       "whip": "1.34",
       "qs": ""
