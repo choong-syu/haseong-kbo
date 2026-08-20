2984dc8837756996ffa16b71dd7eb9014d76cf87
choong-syu <128004136+choong-syu@users.noreply.github.com>
2026-08-20 01:53:37 +0000

    Update KBO data

--- data/team-stats.js
+++ data/team-stats.js
@@ -1,7 +1,7 @@
 window.KBO_TEAM_STATS = {
   "season": 2026,
-  "asOf": "2026년 8월 20일 09시 기준",
-  "fetchedAt": "2026-08-20T00:01:22.210Z",
+  "asOf": "2026년 8월 20일 10시 기준",
+  "fetchedAt": "2026-08-20T01:53:36.577Z",
   "source": {
     "standings": "https://eng.koreabaseball.com/Standings/TeamStandings.aspx",
     "hitter": "https://m.koreabaseball.com/Kbo/Record/TeamRecord_hitter.aspx",
