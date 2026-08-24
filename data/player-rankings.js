c3f0d18ea874b4006276302fc688125966ef9121
choong-syu <128004136+choong-syu@users.noreply.github.com>
2026-08-24 10:55:59 +0000

    Update KBO data

--- data/player-rankings.js
+++ data/player-rankings.js
@@ -1,7 +1,7 @@
 window.KBO_PLAYER_RANKINGS = {
   "season": 2026,
   "asOf": "2026년 8월 24일 19시 기준",
-  "fetchedAt": "2026-08-24T10:06:41.210Z",
+  "fetchedAt": "2026-08-24T10:55:58.986Z",
   "source": {
     "hitterBasic": "https://eng.koreabaseball.com/Stats/BattingLeaders.aspx",
     "hitterDetail": "https://eng.koreabaseball.com/Stats/BattingLeaders02.aspx",
